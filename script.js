919a24bb755777edc00ce1e925407cb0b327df98
sutin <injitt>
2023-04-04 16:04:36 +0700

    fix: convert file blob & rename

--- script.js
+++ script.js
@@ -6,16 +6,22 @@ navigator.serviceWorker
   .catch(console.error);
 
 const inp = document.querySelector('input');
-inp.onchange = (e) => {
+inp.onchange = async (e) => {
   const file = inp.files[0];
-  displayRenamedPDF(file, 'new-name.pdf').then(console.log);
+  const reader = new FileReader();
+  reader.onload = function(e) {
+      const blob = new Blob([new Uint8Array(e.target.result)], {type: file.type });
+      displayRenamedPDF(blob, 'myPdf.pdf').then(console.log);
+  };
+  reader.readAsArrayBuffer(file);
+  
 };
 
 async function displayRenamedPDF(file, filename) {
   // we use an hard-coded fake path
   // to not interfere with legit requests
   const reg_path = '/name-forcer/';
-  const url = reg_path + 'new-name.pdf';
+  const url = reg_path + filename;
   const store = await caches.open('name-forcer');
   await store.put(url, new Response(file));
   const frame = document.createElement('iframe');
@@ -23,6 +29,7 @@ async function displayRenamedPDF(file, filename) {
   frame.style.height = '100vh';
   document.body.append(frame);
   frame.src = url;
+  window.open(url, '_blank')
 }
 // clear previously cached files
 (async () => {
